e2648877abb145a950c189af961a44227abd8ebb
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-09-23 10:32:03 +0100

    feat: use reducer

--- src/contexts/todo.tsx
+++ src/contexts/todo.tsx
@@ -1,40 +1,38 @@
-import { createContext, useState } from "react";
+import type { Todo, TodoContextType, TodoPayload } from "./todo.types";
 
-export interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
+import { createContext, useReducer } from "react";
 
-interface TodoContextType {
-  todos: Todo[];
-  addTodo: (text: string) => void;
-  toggleTodo: (id: number) => void;
-  removeTodo: (id: number) => void;
+function todoReducer(state: Todo[], action: { type: string; payload: TodoPayload }): Todo[] {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.payload];
+    case 'TOGGLE_TODO':
+      return state.map(todo =>
+        todo.id === action.payload.id ? { ...todo, completed: !todo.completed } : todo
+      );
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.payload.id);
+    default:
+      return state;
+  }
 }
 
 const TodoContext = createContext<TodoContextType | undefined>(undefined);
 
 const TodoProvider = ({ children }: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(todoReducer, []);
 
   const addTodo = (text: string) => {
-    const newTodo: Todo = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
+    const [id, completed] = [Date.now(), false];
+    dispatch({ type: 'ADD_TODO', payload: { id, text, completed } });
   };
 
   const toggleTodo = (id: number) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ));
+    dispatch({ type: 'TOGGLE_TODO', payload: { id, text: '', completed: false } });
   };
 
   const removeTodo = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE_TODO', payload: { id, text: '', completed: false } });
   };
 
   return (
---
+++ src/contexts/todo.types.ts
@@ -0,0 +1,18 @@
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export interface TodoPayload {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export interface TodoContextType {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
