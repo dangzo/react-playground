5d19bd954da778f461933774b73f2335552e02a2
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-10-09 12:10:03 +0100

    fix: zustand list name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
         <div className="flex flex-col w-full p-20">
           <header>
-            <h1 className="text-left mb-8">Context API</h1>
+            <h1 className="text-left mb-8">Zustand</h1>
           </header>
           <TodoListZustand />
         </div>
