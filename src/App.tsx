16689c96a764c16efb3cd96cc2db6c34370185aa
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-10-09 11:30:07 +0100

    feat: double list view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
-import { TodoList } from './components/TodoList'
+import { TodoList as TodoListContext } from './components/TodoListContext/TodoList'
 import { TodoProvider } from './contexts/todo'
 
 function App() {
   return (
     <TodoProvider>
-      <TodoList />
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col w-full p-20">
+          <header>
+            <h1 className="text-left mb-8">Context API</h1>
+          </header>
+          <TodoListContext />
+        </div>
+        <div className="flex flex-col w-full p-20">
+          <header>
+            <h1 className="text-left mb-8">Context API</h1>
+          </header>
+          <TodoListContext />
+        </div>
+      </div>
     </TodoProvider>
   )
 }
